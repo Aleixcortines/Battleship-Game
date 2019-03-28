c32342362f9db323d94ce267dc481afc1de69796
aleixcortines <aleixcortines@gmail.com>
2019-03-28 22:32:22 +0100

    add ships done

--- src/main/java/com/aleixbattleship/salvo/SalvoController.java
+++ src/main/java/com/aleixbattleship/salvo/SalvoController.java
@@ -1,9 +1,13 @@
 package com.aleixbattleship.salvo;
 
+import com.sun.xml.internal.bind.v2.model.core.ID;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.security.PermitAll;
 import java.util.HashMap;
 import java.util.LinkedHashMap;
 import java.util.List;
@@ -21,6 +25,9 @@ public class SalvoController {
     //Attributes - (Autowired annotation: gets the instance needed, in this case, the Repositories where the instances are saved)
     @Autowired
     private GameRepository gameRepository;
+    @Autowired
+    private GamePlayerRepository gamePlayerRepository;
+
 
     //Request Methods - (RequestMapping: specifies the URL where the data is displayed)
     @RequestMapping("/games")
@@ -33,29 +40,70 @@ public class SalvoController {
         }
 
     private Map<String,Object> getAll(Game game){
-        Map<String, Object> mapId = new HashMap<>();
+        Map<String, Object> dto = new HashMap<>();
 
-        mapId.put("id",game.getId());
-        mapId.put("created",game.getDate());
-        mapId.put("gamePlayers",game.getPlayers()
+        dto.put("id",game.getId());
+        dto.put("created",game.getDate());
+        dto.put("gamePlayers",game.getPlayers()
                 .stream().
                         map(Player -> getAll(Player))
                                 .collect(toList()));
-        return mapId;
+        return dto;
     }
-
-
 
     private Map<String, Object> getAll(Player Player) {
-        Map<String, Object>  mapId = new LinkedHashMap<>();
-        mapId.put("id", Player.getId());
-        mapId.put("email", Player.getUserName());
-        return mapId;
+        Map<String, Object>  dto = new LinkedHashMap<>();
+        dto.put("id", Player.getId());
+        dto.put("email", Player.getUserName());
+        return dto;
+    }
+
+       @RequestMapping("/game_view/{gamePlayerID}")
+        public Map <String,Object> gameViewId(@PathVariable Long gamePlayerID) {
+        Map<String,Object> gameViewDTO = new LinkedHashMap<>();
+        GamePlayer gamePlayer= gamePlayerRepository.getOne(gamePlayerID);
+        gameViewDTO.put("game", getGameViewInfo(gamePlayer));
+            return gameViewDTO;
     }
 
 
+    private Map <String,Object> getGPInfo(GamePlayer gamePlayer){
 
+        Map <String,Object> dtoGamePlayerInfo= new HashMap<>();
 
+        dtoGamePlayerInfo.put("id",gamePlayer.getId());
+        dtoGamePlayerInfo.put("player",getAll(gamePlayer.getPlayer()));
 
+        return dtoGamePlayerInfo;
     }
+
+    private Map <String,Object> getGameViewInfo (GamePlayer gamePlayer){
+        Map<String,Object> gameInfo = new HashMap<>();
+
+        gameInfo.put("id",gamePlayer.getGame().getId());
+        gameInfo.put("created",gamePlayer.getGame().getDate());
+        gameInfo.put("Game Player",gamePlayer.getGame().getGamePlayers()
+                .stream()
+                .map(gp -> getGPInfo(gp))
+                .collect(toList()));
+
+        gameInfo.put("Ships",gamePlayer.getShip()
+                .stream()
+                .map(ship -> makeShipDTO(ship))
+                .collect(toList())
+
+        );
+
+        return gameInfo;
+    }
+
+    private Map <String,Object> makeShipDTO (Ship ship){
+        Map <String,Object> shipDTO = new HashMap<>();
+        shipDTO.put("Type",ship.getType());
+        shipDTO.put("Location",ship.getLocations());
+        return shipDTO;
+    }
+
+}
+
 
