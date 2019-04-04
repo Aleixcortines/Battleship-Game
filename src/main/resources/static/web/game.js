20de5baa67410de9c1a073d776dc9d3722e4cacb
aleixcortines <aleixcortines@gmail.com>
2019-04-04 19:50:27 +0200

    add scores

--- src/main/resources/static/web/game.js
+++ src/main/resources/static/web/game.js
@@ -1,7 +1,8 @@
 let app = new Vue({
     el: "#app",
     mounted() {
-        this.populateTable();
+        this.populateTable("table1");
+        this.populateTable("table2");
         this.getID();
         this.getData();
 
@@ -13,7 +14,9 @@ let app = new Vue({
         gamePlayers: [],
         games: [],
         ships: [],
-        gameDat:[],
+        gameDat: [],
+        salvoOpponent: [],
+        mySalvoes:[],
     },
 
     methods: {
@@ -28,16 +31,20 @@ let app = new Vue({
                     return response.json()
                 })
                 .then(function (gameJson) {
-                
-                    app.gameDat=gameJson;
+
+                    app.gameDat = gameJson;
 
                     app.games = gameJson.game.GamePlayer;
 
                     app.ships = gameJson.game.Ships;
 
-                    app.printShipsGrid();
+                    app.salvoOpponent = gameJson.game.opponentSalvoes;
+                
+                    app.mySalvoes =  gameJson.game.mySalvoes;
 
                     app.gameVs();
+
+                    app.printSalvoesOpponent();
 
 
                 })
@@ -54,23 +61,26 @@ let app = new Vue({
 
             this.gamePlayers = vars;
         },
-
-        populateTable() {
+        //funtion to crate the grid
+        populateTable(tablesHTML) {
+    
 
             const column = ["", "A", "B", "C", "D", "E", "F", "G", "H", "I", "J"]
-            const body = document.getElementById("table");
+            const tables = document.getElementById(tablesHTML);
             const table = document.createElement("table");
             const tbody = document.createElement("tbody");
 
             //the element above will be use later
 
             table.appendChild(tbody);
-
+            
             //bind the tbody  to the table element, the tbody will be then build with the 2 loops below
 
             for (var i = 0; i < 11; i++) {
+                
 
                 var tr = document.createElement("tr");
+                
                 tbody.appendChild(tr);
 
                 //bind the tr element to the tbody, the tr will be built below:
@@ -87,73 +97,122 @@ let app = new Vue({
                         td.textContent = column[i];
                     } //this will add the content of the first row(the numbers)
 
-                    td.setAttribute("id", column[i] + j);
+                    td.setAttribute("id", column[i] + j+tablesHTML);
                     //this will add an ID to match each cell (eg: cell A1 will have the ID="A1" )
 
                     tr.appendChild(td);
                     //this bind the builded cells to the rows, whichare themselves built at the first loop
                 }
             }
-            body.appendChild(table);
+            tables.appendChild(table);
             //td bind to  tr
             // tr bind to tbody
             //tbody bind to table
             // and now, we are pushing the table to the body , which is the ID of the div in HTMl page
         },
-        //function to print the ships on the grid
-        printShipsGrid() {
+       
+        gameVs() {
+            
+          
 
+            let playersGameId = document.getElementById("playersGame");
+
+            if (this.gameDat.game.GamePlayer.length == 1) {
+
+
+                let playerOne = this.gameDat.game.GamePlayer[0].player.email;
+
+
+                playersGameId.textContent = playerOne + " VS Waiting for a opponent "
+
+            } else {
+
+                let playerOne = this.gameDat.game.GamePlayer[0].player.email;
+                let playerTwo = this.gameDat.game.GamePlayer[1].player.email;
+
+                if (this.gameDat.game.GamePlayer[0].id == this.gamePlayers) {
+
+                    playersGameId.textContent = playerOne + " (YOU) vs " + playerTwo;
+
+                } else {
+
+                    playersGameId.textContent = playerTwo + " (YOU) vs " + playerOne;
+
+                }
+
+            }
+
+        },
+
+
+        printSalvoesOpponent() {
+            
+            
+            var shipLocationArray = [];
+           
+  
+            for (var s=0 ; s<this.mySalvoes.length; s++){
+                
+                var mySalvoLocation = this.mySalvoes[s].Location;
+                
+                var mySalvoTurn = this.mySalvoes[s].Turn;
+                
+                
+                for ( var l = 0 ; l<mySalvoLocation.length;l++){
+                    
+                    document.getElementById(mySalvoLocation[l]+"table2").style.backgroundColor= 'yellow'
+                    
+                     document.getElementById(mySalvoLocation[l]+"table2").innerHTML= mySalvoTurn;
+                }
+                
+            }
+            
+            
             for (var i = 0; i < this.ships.length; i++) {
 
                 var locations = this.ships[i].Location;
 
                 for (var j = 0; j < locations.length; j++) {
 
-                    document.getElementById(locations[j]).style.backgroundColor = '#451d00';
+                    document.getElementById(locations[j]+"table1").style.backgroundColor = '#424245'
+
+                    shipLocationArray.push(locations[j]);
+                    
 
                 }
             }
 
-        },
-        
-        gameVs(){
-            
-            let playersGameId= document.getElementById("playersGame");
-            
-            if (this.gameDat.game.GamePlayer.length == 1){
+            for (var i = 0; i < this.salvoOpponent.length; i++) {
                 
+
+                var salvoLocations = this.salvoOpponent[i].Location;
                 
-                let playerOne = this.gameDat.game.GamePlayer[0].player.email;
-                
-                
-                playersGameId.textContent = playerOne + " ----- VS Waiting for a opponent "
-                
-            } else {
-                
-                let playerOne = this.gameDat.game.GamePlayer[0].player.email;
-                let playerTwo = this.gameDat.game.GamePlayer[1].player.email;
-                
-                if ( this.gameDat.game.GamePlayer[0].id == this.gamePlayers){
+                var salvoTurnOpp = this.salvoOpponent[i].Turn;
+
+                for (var j = 0; j < salvoLocations.length; j++) {
+
+                    document.getElementById(salvoLocations[j]+"table1").style.backgroundColor = 'yellow'
+                    document.getElementById(salvoLocations[j]+"table1").innerHTML= salvoTurnOpp;
                     
-                    playersGameId.textContent = playerOne + " (YOU) vs " + playerTwo;
-                    
-                } else{
-                    
-                     playersGameId.textContent = playerTwo + " (YOU) vs " + playerOne;
-                    
+
+                    for (var x=0; x<shipLocationArray.length;x++) {
+                        
+                        if (salvoLocations[j].includes(shipLocationArray[x])) {
+                            
+                            document.getElementById(salvoLocations[j]+"table1").style.backgroundColor='red'
+                            
+                        }
+
+                    }
+
                 }
-     
-                
             }
-        
             
+            
+            
+            
+
         }
-
-
-
     }
 
-
 })
-
-
