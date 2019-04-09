241ecba6d4929dace59eee615aba9ba582dc86cf
aleixcortines <aleixcortines@gmail.com>
2019-04-09 21:58:22 +0200

    table done

--- src/main/resources/static/web/games.js
+++ src/main/resources/static/web/games.js
@@ -1,51 +1,95 @@
-//var dataVue = new Vue({
-//    el: '#app',
-//    //on mounted We put this function is to call my json (on myjason.com url)
-//    mounted() {
-//        this.cargarPage();
-//    },
-//    // This is the model.
-//    data: {
-//
-//        games:[],
-//    },
-//    // Functions we will be using.
-//    methods: {
-//        //function to call the json on myjason.com
-//        cargarPage() {
-//
-//            this.$http.get('/api/games')
-//                .then((response) => {
-//                    //on this variables I put the values of my jsn
-//                    this.games=response.body;
-//
-//                });
-//        },
-//    },
-//});
-
 let app = new Vue({
     el: "#app",
     mounted() {
         this.getDataFunction();
+
     },
     data: {
-        gameData: []
+        gameData: [],
+        gameScores: [],
+        leaderBoard: []
+
     },
     methods: {
-    getDataFunction() {
-        let url = "http://localhost:8080/api/games";
+        getDataFunction() {
+            var url = "http://localhost:8080/api/games";
 
-        fetch(url, {
-            mode: "cors"
-        })
-        .then(function(response){
-            return response.json()
-        })
-        .then(function(gameJson){
-            app.gameData = gameJson
-        })
-        .catch(error => console.log(error))
-    }
+            fetch(url, {
+                    mode: "cors",
+                })
+                .then(function (response) {
+                    return response.json();
+                })
+                .then(function (gameJson) {
+                    app.gameData = gameJson;
+                    app.getScores();
+                    app.createTable();
+                })
+        },
+
+        //function to get players and scores and push on gameScores
+        getScores() {
+
+            let games = app.gameData;
+
+            for (let i = 0; i < games.length; i++) {
+                let sc = games[i].scores;
+                this.gameScores.push(sc);
+
+            }
+        },
+        createTable() {
+            let players = this.gameScores;
+            //unique player's name
+            let filtered = [];
+            //each match score
+            let playerScores = [];
+            //filtered players with their score
+            let pl = [];
+
+            for (let i = 0; i < players.length; i++) {
+                players[i].map(score => {
+                    playerScores.push(score)
+                    if (!filtered.includes(score.player)) {
+                        filtered.push(score.player)
+                        pl.push({
+                            name: score.player,
+                            win: 0,
+                            lose: 0,
+                            tie: 0,
+                            pts: 0,
+                        })
+                    }
+                })
+            }
+            
+            console.log(pl);
+
+            for (let i = 0; i < filtered.length; i++) {
+                for (let j = 0; j < playerScores.length; j++) {
+                    if (filtered[i] == playerScores[j].player) {
+
+                        pl[i].pts += playerScores[j].score;
+
+                        switch (playerScores[j].score) {
+                            case 1.0:
+                                pl[i].win++
+                                break;
+                            case 0.5:
+                                pl[i].tie++
+                                break;
+                            case 0.0:
+                                pl[i].lose++
+                                break;
+                        }
+                    }
+                }
+            }
+            
+            
+            let sortedArray = pl.sort((a, b) => a.pts < b.pts ? 1 : a.pts > b.pts ? -1 : 0);
+            app.leaderBoard = sortedArray;
+        }
+
     }
 })
