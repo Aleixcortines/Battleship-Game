b45ea1ac6198555b4785a3bebb256a88f091a170
aleixcortines <aleixcortines@gmail.com>
2019-04-12 14:20:52 +0200

    sign up done

--- src/main/resources/static/web/games.js
+++ src/main/resources/static/web/games.js
@@ -7,8 +7,10 @@ let app = new Vue({
     data: {
         gameData: [],
         gameScores: [],
-        leaderBoard: []
-
+        leaderBoard: [],
+        emailInput: "",
+        passwordInput: "",
+        player: null
     },
     methods: {
         getDataFunction() {
@@ -21,39 +23,99 @@ let app = new Vue({
                     return response.json();
                 })
                 .then(function (gameJson) {
-                    app.gameData = gameJson;
+
+                    app.gameData = gameJson.games;
+                    app.player = gameJson.player
                     app.getScores();
                     app.createTable();
                 })
-        },
-
-        login(evt) {
-            evt.preventDefault();
-            var form = evt.target.form;
-            $.post("/api/login", {
-                    name: form["username"].value,
-                    pwd: form["password"].value
+                .catch(function (error) {
+                    console.log("error  " + error.message)
                 })
-                .done()
-                .fail();
         },
 
-        logout(evt) {
-            evt.preventDefault();
-            $.post("/api/logout")
-                .done()
-                .fail();
+        getBody(user) {
+            var body = [];
+            for (var key in user) {
+                var encKey = encodeURIComponent(key);
+                var encVal = encodeURIComponent(user[key]);
+                body.push(encKey + "=" + encVal);
+            }
+            return body.join("&");
+        },
+
+        login() {
+
+            fetch("/api/login", {
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/x-www-form-urlencoded'
+                    },
+                    method: 'POST',
+                    body: this.getBody({
+                        username: this.emailInput,
+                        password: this.passwordInput
+                    })
+                })
+                .then(function (response) {
+                    if (response.status >= 401) {
+                        alert("Username or password invalid")
+                    } else {
+                        location.reload(true);
+                    }
+                })
+                .catch(function (error) {
+                    console.log('Request failure: ', error);
+                });
+        },
+
+        signup() {
+            fetch("/api/players", {
+                    credentials: 'include',
+                    headers: {
+                        'Content-Type': 'application/json'
+                    },
+                    method: 'POST',
+                    body: JSON.stringify({
+                        userName: this.emailInput,
+                        password: this.passwordInput
+                    })
+                })
+                .then(function (response) {
+                    console.log('Request success: ', response);
+
+
+                }).then(function (data) {
+
+                    console.log(data);
+                    app.login()
+
+                }).catch(function (error) {
+                    console.log('Request failure: ', error);
+                });
+
+        },
+
+        logout() {
+            fetch("/api/logout", {
+                    method: "POST",
+                })
+                .then(function (gameJson) {
+                    console.log("Log out success")
+                })
+                .then(function () {
+                    location.reload(true)
+                })
+                .catch(error => console.log(error))
         },
 
         //function to get players and scores and push on gameScores
         getScores() {
 
             let games = app.gameData;
-
             for (let i = 0; i < games.length; i++) {
                 let sc = games[i].scores;
                 this.gameScores.push(sc);
-
             }
         },
         createTable() {
